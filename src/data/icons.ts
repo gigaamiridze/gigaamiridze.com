d9fd59043f06974ddf7d69dbb77b2f5f738fccf0
justtfelix <justttfelix@gmail.com>
2023-02-27 21:48:52 +0400

    Added social icons data

---
+++ src/data/icons.ts
@@ -0,0 +1,34 @@
+// Importing Icons
+import { TbBrandGithub } from "react-icons/tb";
+import { FiLinkedin } from "react-icons/fi";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { RxTwitterLogo } from "react-icons/rx";
+import { SlSocialFacebook } from "react-icons/sl";
+
+export const icons = [
+  {
+    url: "https://github.com/justtfelix",
+    icon: TbBrandGithub,
+    title: "GitHub",
+  },
+  {
+    url: "https://www.linkedin.com/in/giga-amiridze-755340263",
+    icon: FiLinkedin,
+    title: "LinkedIn",
+  },
+  {
+    url: "https://instagram.com/justttfelix",
+    icon: AiOutlineInstagram,
+    title: "Instagram",
+  },
+  {
+    url: "https://twitter.com/justttfelix",
+    icon: RxTwitterLogo,
+    title: "Twitter",
+  },
+  {
+    url: "https://facebook.com/justttfelix",
+    icon: SlSocialFacebook,
+    title: "Facebook",
+  },
+]
