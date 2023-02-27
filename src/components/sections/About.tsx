fee199cea3c11679dc2562d713f4bda444b35b9c
justtfelix <justttfelix@gmail.com>
2023-02-27 23:13:06 +0400

    Generated about section texts using map

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import uuid from "react-uuid";
+import { texts } from "../../data/aboutTexts";
 // Importing Components
 import AboutContainer from "../containers/AboutContainer";
 import { FlexBox, FlexBlock } from "../Flex";
@@ -18,21 +20,15 @@ function About() {
       </FlexBox>
       <FlexBlock>
         <TextContainer>
-          <AboutText>
-            Hello! My name is Brittany and I enjoy creating things that live on the internet. My 
-            interest in web development started back in 2012 when I decided to try editing custom Tumblr 
-            themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
-          </AboutText>
-          <AboutText>
-            Fast-forward to today, and I’ve had the privilege of working at an advertising agency, 
-            a start-up, a huge corporation, and a student-led design studio. My main focus these days 
-            is building accessible, inclusive products and digital experiences at Upstatement for a variety 
-            of clients.
-          </AboutText>
-          <AboutText>
-            I also recently launched a course that covers everything you need to build a web app with 
-            the Spotify API using Node & React.
-          </AboutText>
+          {texts.map((item) => {
+            const id = uuid();
+
+            return (
+              <AboutText key={id}>
+                {item.text}
+              </AboutText>
+            )
+          })}
         </TextContainer>
       </FlexBlock>
     </AboutContainer>
