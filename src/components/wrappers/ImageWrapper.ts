9128bdc3c69fbac284fedd377aab2374b0c535d9
justtfelix <justttfelix@gmail.com>
2023-03-01 21:15:29 +0400

    Added green outline for portfolio image

--- src/components/wrappers/ImageWrapper.ts
+++ src/components/wrappers/ImageWrapper.ts
@@ -9,6 +9,20 @@ const ImageWrapper = styled.div`
   width: 100%;
   height: 300px;
   border-radius: 5px;
+  position: relative;
+
+  &::after {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    border-radius: 4px;
+    outline: 2px solid ${({theme}) => theme.colors.green};
+    position: absolute;
+    top: 18px;
+    left: 18px;
+    z-index: -1;
+  }
 `;
 
 export default ImageWrapper;
