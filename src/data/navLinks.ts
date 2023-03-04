c0d76bcd9366a2bb1f1c76f1f4d0bf8351be1916
justtfelix <justttfelix@gmail.com>
2023-03-04 19:24:39 +0400

    Added data of nav links

---
+++ src/data/navLinks.ts
@@ -0,0 +1,12 @@
+export const navLinks = [
+  {
+    queueNumber: "01.",
+    address: "hello",
+    title: "Hello",
+  },
+  {
+    queueNumber: "02.",
+    address: "about",
+    title: "About",
+  },
+]
