c65dbb8cbe6941e2204f4378b35453159aa5ae03
justtfelix <justttfelix@gmail.com>
2023-03-06 21:51:05 +0400

    Added github stats inside the footer

---
+++ src/components/GitHubStats.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const GitHubStats = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 20px;
+  margin-top: 12px;
+`;
+
+export default GitHubStats;
--- src/components/contents/Footer.tsx
+++ src/components/contents/Footer.tsx
@@ -1,17 +1,32 @@
 import React from "react";
+// Importing Icons
+import { RiGitRepositoryLine } from "react-icons/ri";
+import { FiGitCommit } from "react-icons/fi";
 // Importing Components
 import FooterContainer from "../containers/FooterContainer";
 import { Link as GitHubLink } from "../Link";
+import GitHubStats from "../GitHubStats";
+import { FlexBox } from "../Flex";
 
 function Footer() {
   return (
     <FooterContainer>
-      <GitHubLink 
-        href="https://github.com/justtfelix" 
-        rel="noopener noreferrer" 
+      <GitHubLink
+        href="https://github.com/justtfelix"
+        rel="noopener noreferrer"
         target="_blank"
       >
         <p>Built by Giga Amiridze</p>
+        <GitHubStats>
+          <FlexBox>
+            <RiGitRepositoryLine title="Repositories" fontSize={14} />
+            <span style={{ marginLeft: 5 }}>43</span>
+          </FlexBox>
+          <FlexBox>
+            <FiGitCommit title="Commits" fontSize={14} />
+            <span style={{ marginLeft: 5 }}>241</span>
+          </FlexBox>
+        </GitHubStats>
       </GitHubLink>
     </FooterContainer>
   )
