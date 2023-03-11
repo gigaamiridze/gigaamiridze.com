ce115e8fe9f9b719fd45198f37128a01b07c0646
justtfelix <justttfelix@gmail.com>
2023-03-11 16:06:55 +0400

    Added featured projects data

---
+++ src/data/featuredProjects.ts
@@ -0,0 +1,28 @@
+import { ElBook, TipCalculator, TodoList } from "../assets";
+
+export const featuredProjects = [
+  {
+    image: ElBook,
+    title: "Sandro's Book",
+    description: "This book is an online edition of the book. The stories told in this book are about technology and the magical abilities of modern man. There is no printed version of this book, the stories told in the book can be read and heard only thanks to technology.",
+    designLink: "https://nokak.github.io/Web-edition-of-Sandro-s-book",
+    githubLink: "https://github.com/justtfelix/Web-edition-of-Sandro-s-book",
+    techList: ["JavaScript", "HTML", "SCSS"],
+  },
+  {
+    image: TipCalculator,
+    title: "Tip Calculator",
+    description: "The purpose of the project is to take values from the input fields and select the tip percentage by pressing a button. After completion, the amount to be paid based on the entered value will be displayed.",
+    designLink: "https://tip-calculator-rc.vercel.app",
+    githubLink: "https://github.com/justtfelix/tip-calculator",
+    techList: ["React", "TypeScript", "Styled Components", "HTML"],
+  },
+  {
+    image: TodoList,
+    title: "Todo List",
+    description: "Todo List App is a kind of app that is generally used to maintain our day-to-day tasks or list everything that we have to do. It helps plan our daily schedules.",
+    designLink: "https://todo-list-rc.vercel.app",
+    githubLink: "https://github.com/justtfelix/todo-list",
+    techList: ["React", "JavaScript", "HTML", "SCSS"],
+  }
+]
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,4 @@
+export * from "./featuredProjects";
 export * from "./aboutTexts";
 export * from "./navLinks";
 export * from "./icons";
