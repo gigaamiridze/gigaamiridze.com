04c9f07426f81d400f354d8515d19c35af32fd9e
justtfelix <justttfelix@gmail.com>
2023-03-19 22:04:42 +0400

    Added hover effect for project box

--- src/components/projects/others/ProjectInfo.ts
+++ src/components/projects/others/ProjectInfo.ts
@@ -9,6 +9,14 @@ const ProjectInfo = styled.div`
   width: 312px;
   padding: 2rem 1.75rem;
   border-radius: 4px;
+
+  &:hover {
+    transform: translateY(-7px);
+
+    #project-title {
+      color: ${({ theme }) => theme.colors.green};
+    }
+  }
 `;
 
 export default ProjectInfo;
--- src/layouts/projects/other/Projects.tsx
+++ src/layouts/projects/other/Projects.tsx
@@ -9,27 +9,30 @@ import {
 function Projects() {
   return (
     <ProjectBoxes>
-      <ProjectInfo>
-        <TopArea>
-          <CiFolderOn title="Folder" />
-          <ProjectLinks githubLink="#" designLink="#" />
-        </TopArea>
-        <OtherProjectTitle
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Integrating Algolia Search with WordPress Multisite
-        </OtherProjectTitle>
-        <OtherDescription>
-          Todo List App is a kind of app that is generally used to maintain our day-to-day tasks or list.
-        </OtherDescription>
-        <OtherTechList>
-          <li>Algolia</li>
-          <li>WordPress</li>
-          <li>PHP</li>
-        </OtherTechList>
-      </ProjectInfo>
+      <a href="#" rel="noopener noreferrer" target="_blank">
+        <ProjectInfo>
+          <TopArea>
+            <CiFolderOn title="Folder" />
+            <ProjectLinks githubLink="#" designLink="#" />
+          </TopArea>
+          <OtherProjectTitle
+            href="#"
+            id="project-title"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Integrating Algolia Search with WordPress Multisite
+          </OtherProjectTitle>
+          <OtherDescription>
+            Todo List App is a kind of app that is generally used to maintain our day-to-day tasks or list.
+          </OtherDescription>
+          <OtherTechList>
+            <li>Algolia</li>
+            <li>WordPress</li>
+            <li>PHP</li>
+          </OtherTechList>
+        </ProjectInfo>
+      </a>
     </ProjectBoxes>
   )
 }
