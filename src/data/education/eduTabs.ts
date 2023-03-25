731f5ac1bfce3b8ee1e4c128d386250ea520ee49
justtfelix <justttfelix@gmail.com>
2023-03-25 18:17:21 +0400

    Added education tabs data

---
+++ src/data/education/eduTabs.ts
@@ -0,0 +1,24 @@
+const eduTabs = [
+  {
+    id: 0,
+    institution: "IT Step Academy",
+  },  
+  {
+    id: 1,
+    institution: "BitCamp",
+  },
+  {
+    id: 2,
+    institution: "Geolab",
+  },
+  {
+    id: 3,
+    institution: "Unilab",
+  },
+  {
+    id: 4,
+    institution: "Unilab",
+  },
+]
+
+export default eduTabs;
---
+++ src/data/education/index.ts
@@ -0,0 +1,1 @@
+export { default as eduTabs } from "./eduTabs";
