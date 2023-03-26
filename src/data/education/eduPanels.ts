0c98605cbd068f8151071af3f395be35b817d710
justtfelix <justttfelix@gmail.com>
2023-03-26 20:30:38 +0400

    Added courses description data

--- src/data/education/eduPanels.ts
+++ src/data/education/eduPanels.ts
@@ -6,9 +6,9 @@ const eduPanels = [
     companyLink: "https://ge.itstep.org",
     range: "October 2022 - Present",
     texts: [
-      "",
-      "",
-      "",
+      "During my studies at the Academy, I will study 7 subjects, such as CompTIA A+, CCNA, Windows Server, Linux Server, Python, CCNP, and Cyber Security",
+      "I complete interesting projects that are as close as possible to reality in the IT industry",
+      "The knowledge and skills I acquire are in line with the current requirements of employers",
     ]
   },
   {
@@ -18,9 +18,9 @@ const eduPanels = [
     companyLink: "https://www.bitcamp.ge",
     range: "—",
     texts: [
-      "",
-      "",
-      "",
+      "I completed BitCamp Pro and BitCamp Reactor courses through the BitCamp YouTube channel",
+      "During the BitCamp Pro course, I learned technologies such as HTML, CSS, JavaScript, React, Node, Express, and databases: PostgreSQL and MongoDB",
+      "During the BitCamp Reactor course, I learned technologies such as React, TypeScript, React Router, React Hook Form, Styled Components, and Framer Motion",
     ]
   },
   {
@@ -28,11 +28,11 @@ const eduPanels = [
     position: "JavaScript/Node.js",
     company: "Geolab",
     companyLink: "https://geolab.edu.ge",
-    range: "January - May 2022",
+    range: "—",
     texts: [
-      "",
-      "",
-      "",
+      "I completed the JavaScript/Node.js course through the Geolab YouTube channel",
+      "In the first part of the course, I learned the basics of JavaScript, modern JavaScript (ES6+), object-oriented programming, and accessing data from third-party APIs",
+      "In the second part of the course, I learned Node.js, its modules, Express.js and MongoDB",
     ]
   },
   {
@@ -42,9 +42,9 @@ const eduPanels = [
     companyLink: "https://unilab.iliauni.edu.ge",
     range: "February - June 2020",
     texts: [
-      "",
-      "",
-      "",
+      "I learned the principles of the website's functionality, built a static website and, if necessary, made changes to the software code of the existing website",
+      "I learned HTML, CSS, JavaScript, and the basic part of UI/UX design",
+      "For the final project at the end of the course, I built a website based on my design",
     ]
   },
   {
@@ -54,9 +54,9 @@ const eduPanels = [
     companyLink: "https://unilab.iliauni.edu.ge",
     range: "February - June 2022",
     texts: [
-      "",
-      "",
-      "",
+      "During the course, I learned the basic concepts of the language, data types and structures, loops, functions, and object-oriented programming",
+      "I learned the Flask framework, SQLAlchemy and Flask RESTful libraries, and SQLite database",
+      "For the final project at the end of the course, I built a Vaccination API",
     ]
   },  
 ]
