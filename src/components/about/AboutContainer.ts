14c1a990ed07fc427afba7c67e885b6e1ae2baf3
justtfelix <justttfelix@gmail.com>
2023-03-28 18:41:44 +0400

    Added animation for about section

--- src/components/about/AboutContainer.ts
+++ src/components/about/AboutContainer.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const AboutContainer = styled.section`
+const AboutContainer = styled(motion.section)`
   max-width: 900px;
 `;
 
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Profile } from "../assets";
+import { fadeIn } from "../animations";
 import { HeadTitle, AboutInfo } from "../layouts";
 import { AboutContainer, FlexBlock, ProfileImgWrapper, Image } from "../components";
 
 function About() {
   return (
-    <AboutContainer id="about">
+    <AboutContainer 
+      id="about"
+      variants={fadeIn("up", 0, 0.3)}
+      initial="hidden"
+      whileInView={"visible"}
+      viewport={{ once: false }}
+    >
       <HeadTitle queueNumber={1} title="About Me" />
       <FlexBlock>
         <AboutInfo />
