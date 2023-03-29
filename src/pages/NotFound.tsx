f58d72341b654924d307b78b68406fcffec0b5c8
justtfelix <justttfelix@gmail.com>
2023-03-29 16:29:20 +0400

    Added document title for not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { tabTitle } from "../utilities";
 
 function NotFound() {
+  useEffect(() => {
+    tabTitle("Page Not Found | Giga Amiridze");
+  }, []);
+  
   return (
     <>Page Not Found!</>
   )
