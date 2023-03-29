7a46bfdde41fdc62f2a31e5d31d5bd3c6a099c9f
justtfelix <justttfelix@gmail.com>
2023-03-29 18:16:51 +0400

    Added animation for 'not found' page

--- src/components/notFound/NotFoundContainer.ts
+++ src/components/notFound/NotFoundContainer.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const NotFoundContainer = styled.section`
+const NotFoundContainer = styled(motion.section)`
   max-width: 335px;
   padding-top: 50px;
   text-align: center;
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { fadeIn } from "../animations";
 import { tabTitle } from "../utilities";
 import { NotFoundContainer, StatusCode, OopsTitle, NotFoundTitle, SectionButton } from "../components";
 
@@ -9,7 +10,11 @@ function NotFound() {
   }, []);
   
   return (
-    <NotFoundContainer>
+    <NotFoundContainer
+      variants={fadeIn("up", 100, 0.3)}
+      initial="hidden"
+      animate="visible"
+    >
       <StatusCode>404</StatusCode>
       <OopsTitle>Ooops...</OopsTitle>
       <NotFoundTitle>page not found</NotFoundTitle>
