f998ba6ebb0984ef3f6910c35c6cc5ab5dfd2d2e
justtfelix <justttfelix@gmail.com>
2023-04-01 16:50:15 +0400

    Added preloader responsive design

--- src/components/preloader/Atom.ts
+++ src/components/preloader/Atom.ts
@@ -17,6 +17,26 @@ const Atom = styled.div`
     background: transparent;
     box-shadow: inset 0 0 10px ${({ theme }) => theme.colors.white};
   }
+
+  @media (max-width: 768px) {
+    width: 270px;
+    height: 270px;
+  }
+
+  @media (max-width: 500px) {
+    width: 250px;
+    height: 250px;
+  }
+
+  @media (max-width: 375px) {
+    width: 230px;
+    height: 230px;
+  }
+
+  @media (max-width: 320px) {
+    width: 210px;
+    height: 210px;
+  }
 `;
 
 export default Atom;
