533599ad86fc5cd9c79af61461ff55d0b85eddb1
justtfelix <justttfelix@gmail.com>
2023-06-29 19:21:07 +0400

    Modified instagram url

--- src/data/icons.ts
+++ src/data/icons.ts
@@ -19,7 +19,7 @@ const icons = [
   },
   {
     id: 3,
-    url: "https://instagram.com/feliixxmj",
+    url: "https://instagram.com/giga.amiridze",
     icon: AiOutlineInstagram,
     title: "Instagram",
   },
