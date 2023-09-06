3d31eb01d69711ceb61cb5a01dba4300699a410e
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-06 11:00:48 +0400

    Modified my experience information

--- src/data/experience/jobPanels.ts
+++ src/data/experience/jobPanels.ts
@@ -6,9 +6,9 @@ const jobPanels = [
     companyLink: "https://unilab.iliauni.edu.ge",
     range: "June - October 2022",
     texts: [
-      "Write modern, performant, maintainable code for a diverse array of client and internal projects",
-      "Work with a programming languages, platforms, frameworks, and version control system such as JavaScript, TypeScript, React, and Git",
-      "Worked closely with designers and management team to develop, document, and manage the websites",
+      "Write modern, performant, maintainable code for a diverse array of client and internal projects.",
+      "Collaborate with designers, project managers, and other developers to transform creative concepts into production realities for clients and stakeholders.",
+      "Work with programming languages, platforms, frameworks, and version control system such as JavaScript, TypeScript, React, and Git.",
     ]
   },
   {
@@ -18,9 +18,9 @@ const jobPanels = [
     companyLink: "https://unilab.iliauni.edu.ge",
     range: "June - October 2022",
     texts: [
-      "Developed and maintained code for in-house and client websites primarily using Python, Flask, SQLAlchemy, and Jinja",
-      "Interfaced with user experience other developers to ensure thoughtful and coherent user experiences across websites",
-      "Communicate with multi-disciplinary teams of designers, producers, and clients on a daily basis",
+      "Deliver high-quality, robust production code for a diverse array of projects for clients primarily using Python, Flask, SQLAlchemy, and Jinja.",
+      "Worked closely with developers and management team to develop, document, and manage the websites.",
+      "Interfaced with user experience other developers to ensure thoughtful and coherent user experiences across websites.",
     ]
   },
 ]
